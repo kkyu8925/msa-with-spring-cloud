b5fc3b4e5158dcf212f4010a8321e0f99479aa60
gusrb8925 <gusrb8925@naver.com>
2021-07-28 21:21:47 +0900

    21-07-28 update user-service, spring security

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     // 사용자 조회
     UserEntity findByUserId(String userId);
+    // 사용자 이메일 조회
+    UserEntity findByEmail(String username);
 }
